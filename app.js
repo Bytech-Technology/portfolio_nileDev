2a2886f90a03617284cc56a8f75f37058c2cac79
Bytech-Technology <bytechsuppor@gmail.com>
2025-11-09 23:59:44 -0300

    set view engine and views directory in app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path'); 
 const routes = require('./routes/routes');
 const dotenv = require('dotenv');
 dotenv.config();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public/'));
 app.use(express.static('assets/'));
 app.use('/', routes);
